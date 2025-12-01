b653c0383564e6f38d2ede44c061ae06bf4e56ab
jocooper <jocooper2018@gmail.com>
2025-12-01 15:26:24 +0100

    suite modele

--- modele.py
+++ modele.py
@@ -1,14 +1,29 @@
 import numpy as np
+import pandas as pd
 
-def decision(discipline: dict[str, int], name: str)-> str:
-    medals:list[int] = [nb_medals for nb_medals in discipline.values()]
+
+def decision(discipline: dict[str, int], name: str) -> None:
+    medals: list[int] = [nb_medals for nb_medals in discipline.values()]
     nb_country = len(discipline.keys())
-    std = np.std(medals) #ecart type
-    print(f"Nombre de pays {nb_country} : ecart-type {std}")
-    return 0
+    std = np.std(medals)  # ecart type
+    print(f"{name}\n\tNombre de pays : {nb_country}\n\tÉcart-type : {std}")
 
-swimming: dict[str, int] = {
-    "HUN" : 1,
-    "GRE" : 7 ,
-    "AUT" : 2
-}
+
+def main() -> None:
+    df = pd.read_csv("everything.csv")
+    disciplines: dict[str, dict[str, int]] = {}
+    for _, row in df.iterrows():
+        if row["Discipline"] not in disciplines.keys():
+            disciplines[row["Discipline"]] = {}
+        if row["Country"] not in disciplines[row["Discipline"]]:
+            disciplines[row["Discipline"]][row["Country"]] = 0
+        disciplines[row["Discipline"]][row["Country"]] += 1
+
+    for discipline in disciplines.keys():
+        decision(disciplines[discipline], discipline)
+
+    print(f"{len(disciplines.keys())} disciplines")
+
+
+if __name__ == "__main__":
+    main()
